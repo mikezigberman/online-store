26d22586eb96ed89b3b1d4763e40933e65b6a9bf
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-12 17:21:51 +0100

    store(working with Django ORM): display photos in templates

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from products.views import index, products
 
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
