84fd74483fa1421df775b905e8aff308a05156a8
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-12 15:17:14 +0100

    store(working with Django ORM): creation of the first models

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
