ddd858f6bb6107aac4ae692640627f01a9e06f71
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-12 15:51:16 +0100

    store(working with Django ORM): work with models through the admin panel

--- store-server/store/products/models.py
+++ store-server/store/products/models.py
@@ -4,6 +4,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
@@ -11,3 +14,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Product: {self.name} | Category: {self.category.name}'
