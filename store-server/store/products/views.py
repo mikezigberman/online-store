347babfbb8ffbb6bfbbd0ce4c7a2b29df228f769
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-12 16:17:14 +0100

    store(working with Django ORM): displaying models in templates

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import ProductCategory, Product
+
 def index(request):
     context = {'title': 'Store'}
     return render(request, 'products/index.html', context)
@@ -7,25 +9,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Catalog',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Adidas Originals Black Monogram Hoodie',
-                'price': 100,
-                'description': 'Soft fabric for sweatshirts. Style and comfort is a way of life.',
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'The North Face blue jacket',
-                'price': 100,
-                'description': 'Smooth fabric. Waterproof coating. Lightweight and warm down fill.',
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'ASOS DESIGN oversized sports top in brown',
-                'price': 100,
-                'description': 'Plush material. Comfortable and soft.',
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
