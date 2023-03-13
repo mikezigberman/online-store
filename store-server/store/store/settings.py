baafb828605b7a1f4131fd4722ee798a40db7c6b
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-13 15:57:47 +0100

    store(authorization/registration): create application users

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
