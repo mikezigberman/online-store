bcffff712ee5f6a2f3c7d002b6fe310f75c61fc6
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-13 18:03:45 +0100

    store(authorization/registration): authorization controller

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from users.models import User
 from users.forms import UserLoginForm
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 def registration(request):
