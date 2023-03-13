1a396d829735dce2ecf4bf8822b730a26be6b236
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-13 18:58:54 +0100

    store(authorization/registration): own templates and form customization

--- store-server/store/users/forms.py
+++ store-server/store/users/forms.py
@@ -1,8 +1,16 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter password'
+    }))
+
     class Meta:
         model = User
         fields = ('username', 'password')
