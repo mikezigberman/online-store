c2d8dd1e0e616a0ae85efce50d8e03fc3c54225b
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-15 13:47:33 +0100

    store(authorization/registration): final improvements

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -47,3 +47,7 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - Profile', 'form': form}
     return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
