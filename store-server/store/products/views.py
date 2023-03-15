a96ec62571e55d5676ade308efbaabbc05853bad
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-15 16:44:35 +0100

    store(shopping cart): working with access decorators

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import ProductCategory, Product, Basket
-from users.models import User
 
 def index(request):
     context = {'title': 'Store'}
@@ -15,6 +15,7 @@ def products(request):
     }
     return render(request, 'products/products.html', context)
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -28,6 +29,7 @@ def basket_add(request, product_id):
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -34,7 +35,7 @@ def registration(request):
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
