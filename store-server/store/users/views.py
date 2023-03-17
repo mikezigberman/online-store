79f633de62227fc0ad5e731422c2d90d7c999d3c
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-17 17:47:37 +0100

    store(extension of basic functionality (mixins/CBV/email)): adding context processors to the cart

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -33,11 +33,6 @@ class UserProfileView(TitleMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
-
 class EmailVerificationView(TitleMixin, TemplateView):
     title = 'Store - Email confirmation'
     template_name = 'users/email_verification.html'
