d8ba6c3aa60f8634c6e1c703975f89574e480ffc
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-17 18:55:32 +0100

    store(PostgreSQL/OAuth): migrating database to PostgreSQL

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "store_username",
+        "PASSWORD": "store_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
