88dd57285fcf404b78e53bd9d92539209fd0e568
Mike Zigberman <mikezigberman@yahoo.com>
2023-03-17 13:53:46 +0100

    store(extension of basic functionality (mixins/CBV/email)): adding ProductsListView

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
-from django.core.paginator import Paginator
 
 class IndexView(TemplateView):
     template_name = 'products/index.html'
@@ -13,18 +13,21 @@ class IndexView(TemplateView):
         context['title'] = 'Store'
         return context
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    context = {
-        'title': 'Store - Catalog',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context)
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - Catalog'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 @login_required
 def basket_add(request, product_id):
